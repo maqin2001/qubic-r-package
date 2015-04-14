4bdeb61f025f7ae19768114fd090b7b197a93109
zy26 <zy26@21cn.com>
2015-04-14 13:58:23 +0800

    remove friend classes

--- matrix.h
+++ matrix.h
@@ -5,13 +5,11 @@
 #include <string>
 
 class Matrix {
-  friend class FopenMatrix; // TODO: fix me
-  friend class FstreamMatrix; // TODO: fix me
 public:
   const std::vector<std::vector<float> >& get_data_const() const { return data; }
   const std::vector<std::string>& get_row_names() const { return row_names; }
   const std::vector<std::string>& get_col_names() const { return col_names; }
-protected:
+public: // protected: TODO: Fix me
   std::vector<std::string> row_names;
   std::vector<std::string> col_names;
   std::vector<std::vector<float> > data;
