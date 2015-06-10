adfd92a35caad5ed04b0fc62d3f5a5827f913d2a
zy26 <zy26@21cn.com>
2015-06-10 10:53:14 +0800

    remove auto generated files

--- src/rcpp_qubic.cpp
+++ src/rcpp_qubic.cpp
@@ -14,8 +14,7 @@
 
 using namespace Rcpp;
 
-template<typename T>
-NumericMatrix from_vector(const std::vector<std::vector<T>>& result) {
+template<typename T> NumericMatrix from_vector(const std::vector<std::vector<T>>& result) {
   int nc = result.size();
   int nr = result[0].size();
   NumericMatrix m(nr, nc);
@@ -23,20 +22,19 @@ NumericMatrix from_vector(const std::vector<std::vector<T>>& result) {
     const std::vector<T>& result_j = result[j];
     if (result_j.size() != nr) stop("incompatible size");
     for (int i = 0; i < nr; i++) {
-      m(i,j) = result_j[i];
+      m(i, j) = result_j[i];
     }
   }
   return m;
 }
 
-template<typename T>
-std::vector<std::vector<T>> to_vector(const NumericMatrix& matrix) {
+template<typename T> std::vector<std::vector<T>> to_vector(const NumericMatrix& matrix) {
   auto nc = matrix.ncol();
   auto nr = matrix.nrow();
   std::vector<std::vector<T>> result(nc);
   for (int j = 0; j < nc; j++) {
     for (int i = 0; i < nr; i++) {
-      result[j].push_back(matrix(i,j));
+      result[j].push_back(matrix(i, j));
     }
   }
   return result;
@@ -55,11 +53,11 @@ List get_list() {
 List qubic(NumericMatrix matrix) {
   std::vector<Block> result = r_main(to_vector<float>(matrix));
   int number = result.size();
-  
+
   auto x = LogicalMatrix(matrix.nrow(), number);
   auto y = LogicalMatrix(number, matrix.ncol());
-  
-  for(int i = 0; i < number; i++) {
+
+  for (int i = 0; i < number; i++) {
     for (auto it = result[i].genes_order.begin(); it != result[i].genes_order.end(); it++) {
       x(*it, i) = true;
     }
