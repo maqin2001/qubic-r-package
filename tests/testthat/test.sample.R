92528ab69217971c424fdffa45cfcd54121dda28
zy26 <zy26@users.noreply.github.com>
2016-04-07 18:07:40 +0800

    Updating to 0.99.5

--- tests/testthat/test.sample.R
+++ tests/testthat/test.sample.R
@@ -6,7 +6,7 @@ expect_equal(10, 10 + 1e-7)
 
 test <- matrix(rnorm(5000),100,50)
 test[11:20,11:20] <- rnorm(100,3,0.3)
-res<-biclust(test, method=BCQU())
+res <- biclust::biclust(test, method=BCQU())
 expect_true(isS4(res))
 expect_true(is.object(res))
 res
